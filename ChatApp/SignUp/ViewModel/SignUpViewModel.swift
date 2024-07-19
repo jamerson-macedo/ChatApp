d591f69b7136f59bb3031a52a5d56d2bfb19ecab
jamerson-macedo <jamerson_macedo@hotmail.com>
2024-07-19 17:35:15 -0300

    Tela de cadastro criada

---
+++ ChatApp/SignUp/ViewModel/SignUpViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewModel.swift
+//  ChatApp
+//
+//  Created by Jamerson Macedo on 19/07/24.
+//
+
+import Foundation
+class SignUpViewModel :ObservableObject{
+    @Published var name = ""
+    @Published var email = ""
+    @Published var password = ""
+    
+    func signUp(){
+        
+    }
+    
+}
