06f1510ad126966bf10cf0d10c190454054782d1
jamerson-macedo <jamerson_macedo@hotmail.com>
2024-07-19 01:23:46 -0300

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Jamerson Macedo on 19/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
